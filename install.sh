c9ea55a82a3300895884abc2b1d9e001c842db24
xingjian <dotswing@gmail.com>
2024-11-09 22:37:58 +0800

    update: add cred helper for local config git repo

--- install.sh
+++ install.sh
@@ -11,12 +11,16 @@ echo "Installing config-repo..."
 CREPO_REPO=/.config-repo
 sudo mkdir -p $CREPO_REPO
 sudo chown -R `whoami` $CREPO_REPO
+cd $CREPO_REPO
 if [ -v CREPO_REPO_URL_GITHUB ]; then
   git clone https://github.com/$CREPO_REPO_URL_GITHUB $CREPO_REPO
 elif [ -v CREPO_REPO_URL_GITEE ]; then
   git clone https://gitee.com/$CREPO_REPO_URL_GITEE $CREPO_REPO
 elif [ -v CREPO_REPO_URL ]; then
   git clone $CREPO_REPO_URL $CREPO_REPO
+else
+  git init
 fi
+git config --global credential.helper store
 
 echo "Successfully installed crepo!"
